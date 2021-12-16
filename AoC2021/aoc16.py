f2e4f41c350278aebdfc97d3eecf2d88492bea55
tcatsuko <58714618+tcatsuko@users.noreply.github.com>
2021-12-16 14:41:43 -0600

    Day 16: Packet Decoder

--- AoC2021/aoc16.py
+++ AoC2021/aoc16.py
@@ -16,33 +16,23 @@ def expand_data(input_data):
         expanded_data += hex_dict[letter]
     return expanded_data
 
-input_data = test_1
-expanded_data = expand_data(input_data)
-position = 0
-version_sum = 0
 def decode_packet(expanded_data, position=0):
-
     version_sum = 0
     original_position = position
     data_length = len(expanded_data)
     if position + 7 >= data_length:
-        return(0, data_length)
-    
-    # Get type
+        return(0, data_length, [])
     if position + 3 >= len(expanded_data):
-            return (version_sum, len(expanded_data) - original_position)
+            return (version_sum, len(expanded_data) - original_position, [])
     version_number = int(expanded_data[position:position+3], 2)
     version_sum += version_number
     position += 3
-    # Get type
     if position + 3 > len(expanded_data):
         return (version_sum, len(expanded_data) - original_position)
     packet_type = int(expanded_data[position: position+3], 2)
     position += 3
     total_bits = 6
     if packet_type == 4:
-        
-        # For now, just throw away the values until you get to the next packet
         data_bits = expanded_data[position: position + 5]
         number = ''
         while data_bits[0] == '1':
@@ -54,9 +44,10 @@ def decode_packet(expanded_data, position=0):
         number += data_bits[1:]
         number = int(number, 2)
         position += 5
-        
+        num_to_return = number
     else:
-        # I assume that I will have to parse this all out in part 2
+        num_to_return = 0
+        numbers = []
         length_number = expanded_data[position]
         position += 1
         total_bits += 1
@@ -68,72 +59,66 @@ def decode_packet(expanded_data, position=0):
             result = decode_data(expanded_data[position:position+total_length])
             version_sum += result[0]
             position += result[1]
+            numbers += result[2]
         else:
-            
             contained_packets = int(expanded_data[position:position+11], 2)
             position += 11
             for x in range(contained_packets):
                 result = decode_packet(expanded_data, position)
                 version_sum += result[0]
                 position += result[1]
+                numbers += [result[2]]
+        if packet_type == 0:
+            my_sum = 0
+            for item in numbers:
+                my_sum += item
+            num_to_return = my_sum
+        elif packet_type == 1:
+            product = 1
+            for item in numbers:
+                product *= item
+            num_to_return = product
+        elif packet_type == 2:
+            num_to_return = min(numbers)
+        elif packet_type == 3:
+            num_to_return = max(numbers)
+        elif packet_type == 5:
+            if numbers[0] > numbers[1]:
+                num_to_return = 1
+            else:
+                num_to_return = 0
+        elif packet_type == 6:
+            if numbers[0] < numbers[1]:
+                num_to_return = 1
+            else:
+                num_to_return = 0
+        elif packet_type == 7:
+            if numbers[0] == numbers[1]:
+                num_to_return = 1
+            else:
+                num_to_return = 0
     bytes_moved = position - original_position
-    return (version_sum, position - original_position)    
+    return (version_sum, position - original_position, num_to_return)    
 
 def decode_data(expanded_data):
+    numbers = []
     version_sum = 0
     position = 0
     data_length = len(expanded_data)
     while position < len(expanded_data):
-        # Get version number
-        
-            #version_sum += decode(expanded_data[position:position+total_length])[0]
-            result = decode_packet(expanded_data, position)
-            version_sum += result[0]
-            position += result[1]
-            
-            
-    return (version_sum, position)
+        result = decode_packet(expanded_data, position)
+        version_sum += result[0]
+        position += result[1]
+        numbers += [result[2]]
+          
+    return (version_sum, position, numbers)
 
-# Check first example
-
-input_data = 'D2FE28'
-expanded_data = expand_data(input_data)
-result = decode_data(expanded_data)
-print(result[0])
-
-# Now cehck next example
-input_data = '38006F45291200'
-expanded_data = expand_data(input_data)
-result = decode_data(expanded_data)
-print(result[0])
-
-input_data = 'EE00D40C823060'
-expanded_data = expand_data(input_data)
-version_sum, _ = decode_data(expanded_data)
-print(version_sum)
-            
-input_data = '8A004A801A8002F478'
-expanded_data = expand_data(input_data)
-version_sum, _ = decode_data(expanded_data)
-print(version_sum)
-
-input_data = '620080001611562C8802118E34'
-expanded_data = expand_data(input_data)
-version_sum, _ = decode_data(expanded_data)
-print(version_sum)
-
-input_data = 'C0015000016115A2E0802F182340'
-expanded_data = expand_data(input_data)
-version_sum, _ = decode_data(expanded_data)
-print(version_sum)
-
-input_data = 'A0016C880162017C3686B18A3D4780'
-expanded_data = expand_data(input_data)
-version_sum, _ = decode_data(expanded_data)
-print(version_sum)
 
 # Now look at our input
 input_data = raw_input[0]
 expanded_data = expand_data(input_data)
-version_sum, _ = decode_data(expanded_data)
+result = decode_data(expanded_data)
+version_sum = result[0]
 print('Part 1: sum of version numbers is ' + str(version_sum))
+print('Part 2: resulting output is ' + str(result[2][0]))
+
